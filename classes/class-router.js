380b43c2c2622e2040d10892c250491e98997ad1
azrap <azraap@gmail.com>
2019-06-24 15:34:47 -0700

    added get all classes endpoint + functionality

--- classes/class-router.js
+++ classes/class-router.js
@@ -4,6 +4,24 @@ const Classes = require('./class-model.js');
 const restricted = require('../auth/restricted.js');
 
 
+
+
+router.get('/', restricted, async (req, res) => {
+
+    try {
+      const classes = await Classes.findClasses()
+      res.status(200).json( classes);
+    } 
+    catch (error) {
+      // log error to server
+      console.log(error);
+      res.status(500).json({
+        message: 'Error getting the classes',
+      });
+    }
+  });
+
+
 router.get('/:id', restricted, async (req, res) => {
 
     try {
