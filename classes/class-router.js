c76ce17bffd4f4bd2fb3444c3d8a63c260bf4888
azrap <azraap@gmail.com>
2019-06-24 15:12:05 -0700

    added  update a class  + delete by class id

--- classes/class-router.js
+++ classes/class-router.js
@@ -8,7 +8,7 @@ router.get('/:id', restricted, async (req, res) => {
 
     try {
       const classe = await Classes.findClassById(req.params.id)
-      console.log(_class)
+      
 
       res.status(200).json( classe);
     } 
@@ -35,6 +35,37 @@ router.get('/:id', restricted, async (req, res) => {
       console.log(error);
       res.status(500).json({
         message: 'Error adding the class to the database',
+      });
+    }
+  });
+
+  router.delete('/:id', async (req, res) => {
+    try {
+    //   console.log(res)
+    console.log('hello')
+    const classe = await Classes.removeClass(req.params.id);
+      res.status(200).json(classe);
+    } catch (error) {
+      // log error to server
+      console.log(error);
+      res.status(500).json({
+        message: 'Error updating the dang cohorts',
+      });
+    }
+  });
+
+  router.put('/:id', async (req, res) => {
+    try {
+    
+    console.log('hello')
+    const classe = await Classes.updateClass(req.params.id, req.body);
+
+      res.status(200).json(classe);
+    } catch (error) {
+      // log error to server
+      console.log(error);
+      res.status(500).json({
+        message: 'Error updating the dang Class',
       });
     }
   });
