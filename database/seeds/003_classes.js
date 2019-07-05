f2fc5140a52466dc133da48f9de05f4784147ae1
azrap <azraap@gmail.com>
2019-07-05 01:06:51 -0700

    fixing seeding adding instructor seeds

--- database/seeds/003_classes.js
+++ database/seeds/003_classes.js
@@ -5,25 +5,25 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('Classes').insert([
-        {id:1,
+        {
         instructor_id: 1,
         category_id: 1 ,
         name: 'reformer pilates'
         },
 
-        {id:2,
+        {
           instructor_id: 1,
         category_id: 2 ,
         name: 'power yoga'
         },
 
-        {id:3,
+        {
           instructor_id: 2,
         category_id: 2 ,
         name: 'yin yoga',
         },
 
-        {id:4,
+        {
           instructor_id: 2,
         category_id: 3 ,
         name: 'Core40',
