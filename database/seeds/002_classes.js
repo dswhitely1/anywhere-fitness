621d79e46aa536a4e08ba45e919000e0114657d0
azrap <azraap@gmail.com>
2019-07-04 22:04:33 -0700

    fixing seeding classes, switching .truncate to .delete

--- database/seeds/002_classes.js
+++ database/seeds/002_classes.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('Classes').truncate()
+  return knex('Classes').delete()
     .then(function () {
       // Inserts seed entries
       return knex('Classes').insert([
